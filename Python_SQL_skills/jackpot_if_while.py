31bd8b28afc4e2c9c9a53e1599be892e43c39105
Md-Arsh-Ansari <mdarshansari2002@gmail.com>
2022-10-01 17:17:32 +0530

    nothing

--- Python_SQL_skills/jackpot_if_while.py
+++ Python_SQL_skills/jackpot_if_while.py
@@ -2,6 +2,7 @@ import random
 
 jackpot = random.randint(1, 100)
 
+
 guess = int(input("Please guess a number. Sir!\n: "))
 count = 1
 
@@ -19,4 +20,4 @@ while guess != jackpot:
 
 
 print(
-    f"\nBingooo... Sir!!! in just {count} guess!\n Bingo! Bingo! Bingo! \n kamal kar diya Sir aapne to ekdam! \n Dil khush kar diya. \n Wah Sir!! Wah!!!")
+    f"\nBingooo... Sir!!! in just {count} guess!\n Bingo! Bingo! Bingo! \n kamal kar diya Sir aapne to ekdam! \n Dil khush kar diya! \n Wah Sir!! Wah!!!")
